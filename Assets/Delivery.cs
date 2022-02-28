9fd0c20c8dbe1212e10d785f771276216d29198a
Chris Oh <chris@5tigerjelly.com>
2022-02-27 17:15:09 -0800

    changes color when going over package

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     [SerializeField] float destroyDelayTime = 0.5f;
 
@@ -18,6 +27,7 @@ public class Delivery : MonoBehaviour
 
         if (other.tag == "Package" && !hasPackage)
         {
+            spriteRenderer.color = hasPackageColor;
             Debug.Log("I crossed a package");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelayTime);
@@ -29,6 +39,7 @@ public class Delivery : MonoBehaviour
             {
                 Debug.Log("Delivered package");
                 hasPackage = false;
+                spriteRenderer.color = noPackageColor;
             }
             else
             {
