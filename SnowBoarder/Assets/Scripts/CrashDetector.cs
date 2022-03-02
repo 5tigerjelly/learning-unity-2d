480c04bbea95ba34732ac2606f9ffe34d1e488d4
Chris Oh <chris@5tigerjelly.com>
2022-03-01 20:43:44 -0800

    add sound when crash

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,20 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float delayTime = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrashed;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
+            if (!hasCrashed)
+            {
+                hasCrashed = true;
+                GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            }
+
             Invoke("delayScene", delayTime);
         }
     }
